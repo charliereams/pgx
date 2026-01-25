8a51bbac4c51888d68d01424e144c94da094280f
Charlie Reams <charliereams@google.com>
2026-01-25 17:41:43 +0000

    Better decoupling of global state, allowing models of different sizes to compete in the same tournament

--- examples/alphazero/ckpt_to_baseline.py
+++ examples/alphazero/ckpt_to_baseline.py
@@ -37,6 +37,34 @@ from network import AZNet
 from abc import ABC, abstractmethod
 
 
+class TourneyConfig(BaseModel):
+    env_id: pgx.EnvId = "go_9x9"
+    seed: int = 49064405
+    games: int = 256
+
+
+class Config(BaseModel):
+    env_id: pgx.EnvId = "domineering"
+    seed: int = 0
+    max_num_iters: int = 400
+    # network params
+    num_channels: int = 128
+    num_layers: int = 8
+    resnet_v2: bool = True
+    # selfplay params
+    selfplay_batch_size: int = 1024
+    num_simulations: int = 32
+    max_num_steps: int = 256
+    # training params
+    training_batch_size: int = 4096
+    learning_rate: float = 0.001
+    # eval params
+    eval_interval: int = 5
+
+    class Config:
+        extra = "forbid"
+
+
 class MctsConfig(NamedTuple):
     seed: int = 7386708
     num_simulations: int = 30
@@ -66,7 +94,7 @@ class KeyboardAgent(Agent):
                 action_i = self._action_from_square(input("move="))
             elif self.env_id == "g_hex":
                 action_i = self._action_from_ghex_name(input("move="))
-            else
+            else:
                 raise Exception(f"Keyboard parsing not implemented for {self.env_id}")
             #print(f"action_i={action_i} legal={state.legal_action_mask[0][action_i]}")
         return jnp.int32([action_i])
@@ -101,25 +129,63 @@ class RandomAgent(Agent):
     def getAction(self, key, state):
         action_i = None
         while action_i is None or not state.legal_action_mask[0][action_i]:
-            action_i = random.randint(0, 21 * 10) # TODO: select based on env or something
+            action_i = random.randint(0, len(state.legal_action_mask[0]))
         return jnp.int32([action_i])
 
 
 
 class ModelAgent(Agent):
-    def __init__(self, env_id, mcts_config: MctsConfig, model):
+    def __init__(self, name_prefix, env_id, mcts_config: MctsConfig, config: Config, model):
+        def forward_fn(x, is_eval=False):
+            net = AZNet(
+                num_actions=env.num_actions,
+                num_channels=config.num_channels,
+                num_blocks=config.num_layers,
+                resnet_v2=config.resnet_v2,
+            )
+            policy_out, value_out = net(x, is_training=not is_eval, test_local_stats=False)
+            return policy_out, value_out
+
+        forward = hk.without_apply_rng(hk.transform_with_state(forward_fn))
+
+        def recurrent_fn(model, rng_key: jnp.ndarray, action: jnp.ndarray, state: pgx.State):
+            del rng_key
+            model_params, model_state = model
+
+            current_player = state.current_player
+            state = jax.vmap(env.step)(state, action)
+
+            (logits, value), _ = forward.apply(model_params, model_state, state.observation, is_eval=True)
+            # Mask first, then scale for numerical stability.
+            logits = jnp.where(state.legal_action_mask, logits, jnp.finfo(logits.dtype).min)
+            #logits = logits - jnp.max(logits, axis=-1, keepdims=True)
+
+            reward = state.rewards[jnp.arange(state.rewards.shape[0]), current_player]
+            value = jnp.where(state.terminated, 0.0, value)
+            discount = -1.0 * jnp.ones_like(value)
+            discount = jnp.where(state.terminated, 0.0, discount)
+
+            recurrent_fn_output = mctx.RecurrentFnOutput(
+                reward=reward,
+                discount=discount,
+                prior_logits=logits,
+                value=value,
+            )
+            return recurrent_fn_output, state
+
+        self.name_prefix = name_prefix
         self.env_id = env_id
         self.mcts_config = mcts_config
-        self.model = model
-        self.mcts_jit = jax.jit(partial(ModelAgent._run_mcts, mcts_config))
+        self.mcts = partial(ModelAgent._run_mcts, forward, recurrent_fn, mcts_config, model) # Unjitted, for debugging.
+        self.mcts_jit = jax.jit(self.mcts)
 
     def getName(self):
-        return f"Model[sims={self.mcts_config.num_simulations}]"
+        return f"Model[{self.name_prefix}:sims={self.mcts_config.num_simulations}]"
 
     def getAction(self, key, state):
-        ModelAgent._run_mcts(self.mcts_config, self.model, key, state, debug_domineering=(self.env_id == "domineering"), debug_g_hex=(self.env_id == "g_hex"))
+        self.mcts(key, state, debug_domineering=(self.env_id == "domineering"), debug_g_hex=(self.env_id == "g_hex"))
 
-        policy_output = jax.jit(partial(ModelAgent._run_mcts, self.mcts_config))(self.model, key, state)
+        policy_output = self.mcts_jit(key, state)
         if self.env_id == "domineering":
             action_weights = policy_output.action_weights.reshape(8, 8)
             print("\n".join(
@@ -136,7 +202,7 @@ class ModelAgent(Agent):
         return policy_output.action
 
     @staticmethod  # Static for JITting.
-    def _run_mcts(mcts_config, model, key, state, debug_domineering=False, debug_g_hex=False):
+    def _run_mcts(forward, recurrent_fn, mcts_config, model, key, state, debug_domineering=False, debug_g_hex=False):
         key, subkey = jax.random.split(key)
         keys = jax.random.split(subkey, 1)  # Batch size must be 1
         key, subkey = jax.random.split(key)
@@ -175,82 +241,10 @@ class ModelAgent(Agent):
             invalid_actions=~state.legal_action_mask,
             recurrent_fn=recurrent_fn,
             num_simulations=round(mcts_config.num_simulations),
-            #max_depth=32,
             qtransform=mctx.qtransform_completed_by_mix_value,
             gumbel_scale=0.0,
         )
         return policy_output
-
-devices = jax.local_devices()
-num_devices = len(devices)
-
-
-class Config(BaseModel):
-    env_id: pgx.EnvId = "domineering"
-    seed: int = 0
-    max_num_iters: int = 400
-    # network params
-    num_channels: int = 128
-    num_layers: int = 8
-    resnet_v2: bool = True
-    # selfplay params
-    selfplay_batch_size: int = 1024
-    num_simulations: int = 32
-    max_num_steps: int = 256
-    # training params
-    training_batch_size: int = 4096
-    learning_rate: float = 0.001
-    # eval params
-    eval_interval: int = 5
-
-    class Config:
-        extra = "forbid"
-
-
-conf_dict = OmegaConf.from_cli()
-config: Config = Config(**conf_dict)
-print(config)
-
-env = pgx.make(config.env_id)
-
-
-def forward_fn(x, is_eval=False):
-    net = AZNet(
-        num_actions=env.num_actions,
-        num_channels=config.num_channels,
-        num_blocks=config.num_layers,
-        resnet_v2=config.resnet_v2,
-    )
-    policy_out, value_out = net(x, is_training=not is_eval, test_local_stats=False)
-    return policy_out, value_out
-
-
-forward = hk.without_apply_rng(hk.transform_with_state(forward_fn))
-
-def recurrent_fn(model, rng_key: jnp.ndarray, action: jnp.ndarray, state: pgx.State):
-    del rng_key
-    model_params, model_state = model
-
-    current_player = state.current_player
-    state = jax.vmap(env.step)(state, action)
-
-    (logits, value), _ = forward.apply(model_params, model_state, state.observation, is_eval=True)
-    # Mask first, then scale for numerical stability.
-    logits = jnp.where(state.legal_action_mask, logits, jnp.finfo(logits.dtype).min)
-    #logits = logits - jnp.max(logits, axis=-1, keepdims=True)
-
-    reward = state.rewards[jnp.arange(state.rewards.shape[0]), current_player]
-    value = jnp.where(state.terminated, 0.0, value)
-    discount = -1.0 * jnp.ones_like(value)
-    discount = jnp.where(state.terminated, 0.0, discount)
-
-    recurrent_fn_output = mctx.RecurrentFnOutput(
-        reward=reward,
-        discount=discount,
-        prior_logits=logits,
-        value=value,
-    )
-    return recurrent_fn_output, state
 
 
 def pretty_tiles(tiles):
@@ -260,13 +254,20 @@ def pretty_tiles(tiles):
 def load_from_checkpoint(path):
   with open(f"checkpoints/{path}", "rb") as f:
       ckpt = pickle.load(f)
-      return ckpt["model"]
+      return ckpt["config"], ckpt["model"]
 
 if __name__ == "__main__":
-    #model_domineering = load_from_checkpoint("domineering_20260122174624/001100.ckpt")
-    model1 = load_from_checkpoint("g_hex_20260125182112/000100.ckpt")
+    tourney_conf_dict = OmegaConf.from_cli()
+    tourney_config: TourneyConfig = TourneyConfig(**tourney_conf_dict)
+    print(tourney_config)
 
-    env = pgx.make(config.env_id)
+    devices = jax.local_devices()
+
+    #model_domineering = load_from_checkpoint("domineering_20260122174624/001100.ckpt")
+    config1, model1 = load_from_checkpoint("g_hex_20260125182112/000100.ckpt")
+    config2, model2 = load_from_checkpoint("g_hex_20260125222445/000050.ckpt")
+
+    env = pgx.make(tourney_config.env_id)
     init_fn = jax.jit(jax.vmap(env.init))
     step_fn = jax.jit(jax.vmap(env.step))
 
@@ -280,14 +281,14 @@ if __name__ == "__main__":
 
         p1_to_play = True
         while True:
-            if config.env_id == "domineering":
+            if tourney_config.env_id == "domineering":
               print("   abcdefgh")
               print("\n".join(
                   f"{idx+1} |" + "".join("·" if cell else "■" for cell in row) + "|"
                   for idx, row in enumerate(state._x.board.reshape(8, 8))
               ))
               print("")
-            elif config.env_id == "g_hex":
+            elif tourney_config.env_id == "g_hex":
               _TILE_VAL = jnp.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], dtype=jnp.int32)
               print(env.pretty_game(state))
               print("")
@@ -302,9 +303,9 @@ if __name__ == "__main__":
             agent = agents[0 if p1_to_play else 1]
             print(f"{agent.getName()} to play...", flush=True)
             action = agent.getAction(key, state)
-            if config.env_id == "domineering":
+            if tourney_config.env_id == "domineering":
                 print(f"{agent.getName()} played {'abcdefgh'[action[0] % 8]}{1 + (action[0] // 8)}\n")
-            if config.env_id == "g_hex":
+            if tourney_config.env_id == "g_hex":
                 print(f"{agent.getName()} played the {1 + (action[0] % 10)} on triangle {action[0] // 10}\n")
             state = step_fn(state, action)
             p1_to_play = not p1_to_play
@@ -312,16 +313,16 @@ if __name__ == "__main__":
 
     agents = [
         #RandomAgent(),
-        #RandomAgent(),
-        ModelAgent(config.env_id, MctsConfig(seed=33910238, num_simulations=1), model1),
-        ModelAgent(config.env_id, MctsConfig(seed=24364321, num_simulations=1000), model1),
-        #KeyboardAgent(config.env_id),
+        #KeyboardAgent(tourney_config.env_id),
+        ModelAgent("d6", tourney_config.env_id, MctsConfig(seed=33910238, num_simulations=300), config1, model1),
+        ModelAgent("d7", tourney_config.env_id, MctsConfig(seed=12389211, num_simulations=300), config2, model2),
+        #ModelAgent(tourney_config.env_id, MctsConfig(seed=24364321, num_simulations=1000), model1),
     ]
     wins = np.array([0, 0])
-    for game_num in range(0, 4):
+    for game_num in range(0, tourney_config.games):
         agent_1_first = (game_num % 2) == 0
         game_agents = [agents[0], agents[1]] if agent_1_first else [agents[1], agents[0]]
-        print(f"Game {game_num}: {game_agents[0].getName()} vs {game_agents[1].getName()}")
+        print(f"Game {game_num} of {tourney_config.games}: {game_agents[0].getName()} vs {game_agents[1].getName()}")
 
         winner = run_game(game_num, game_agents)
         if winner == (0 if agent_1_first else 1):
@@ -329,8 +330,9 @@ if __name__ == "__main__":
         elif winner == (1 if agent_1_first else 0):
             wins[1] += 1
         win_rates = 100 * wins / (1 + game_num)
-        print(f"""Win rates after {1 + game_num} games:
-                  {agents[0].getName():>20}: {win_rates[0]:6.2f}% ({wins[0]})
-                  {agents[1].getName():>20}: {win_rates[1]:6.2f}% ({wins[1]})
+        print(f"""#######################################################################
+                  Win rates after {1 + game_num} {'game' if game_num == 0 else 'games'}:
+                    {agents[0].getName():>20}: {win_rates[0]:6.2f}% ({wins[0]})
+                    {agents[1].getName():>20}: {win_rates[1]:6.2f}% ({wins[1]})
                """, flush=True)
         time.sleep(10)
